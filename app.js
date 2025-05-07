472ccc975ccb4878f8f449bc647f7edc2e44b504
Sanjyrana <Sanjayrana5113@gmail.com>
2025-05-07 19:00:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("server is running ");
+});
+
+app.listen(3000,()=>{
+    console.log("app is running at port 3000");
+})
